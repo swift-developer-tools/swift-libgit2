b28a3eef851a0630d38d089e8517eee6c8064113
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-03 12:44:58 -0700

    Add platforms to package description

--- Package.swift
+++ Package.swift
@@ -15,6 +15,11 @@ import PackageDescription
 
 let package = Package(
     name: "swift-libgit2",
+    platforms:
+    [
+        .iOS(.v15),
+        .macOS(.v11)
+    ],
     products:
     [
         .library(
