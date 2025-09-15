1146c6c472c652f94dbe708a6f4c1c5f9f10d57f
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-15 14:37:03 -0700

    Add gitOIDEqual binding

---
+++ Sources/swift-libgit2/OID/OID-Functions.swift
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import Clibgit2
+
+
+
+/// Compares two OIDs for equality.
+/// - Parameters:
+///   - a: The first OID.
+///   - b: The second OID.
+/// - Returns: Whether the two OIDs are equal.
+///
+/// ## C Equivalent
+///
+/// [`git_oid_equal()`](https://libgit2.org/docs/reference/main/oid/git_oid_equal.html)
+public func gitOIDEqual(
+    a   : GitOID,
+    b   : GitOID
+) -> Bool
+{
+    var cFirstOID   : git_oid   = a.cValue
+    var cSecondOID  : git_oid   = b.cValue
+    
+    let oidEqualResult: Int32 = git_oid_equal(
+        &cFirstOID,
+        &cSecondOID
+    )
+    
+    return oidEqualResult == 1
+}
