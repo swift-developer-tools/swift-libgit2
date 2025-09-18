8608e4ba9eaf4351fcc6e662ebebcc3795dff1e0
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-18 16:37:13 -0700

    Update GitCertT.init?(cValue:) documentation

--- Sources/swift-libgit2/Cert/Cert-Enums.swift
+++ Sources/swift-libgit2/Cert/Cert-Enums.swift
@@ -43,7 +43,7 @@ public enum GitCertT: UInt32
     
     
     /// Creates a ``GitCertT`` instance from a `git_cert_t` instance.
-    /// - Parameter cValue: The `git_cert_t` instance to use.
+    /// - Parameter cert: The `git_cert_t` instance to use.
     internal init?(
         cValue cert: git_cert_t
     )
