9a41751fdc37f3f462d9bf90715469508a8aa685
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-19 13:37:59 -0700

    Add Transport bindings

---
+++ Sources/swift-libgit2/Transport/Transport-Callbacks.swift
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import Clibgit2
+
+
+
+/// The callback for messages received by the transport.
+/// - Parameters:
+///   - str: The message from the transport.
+///   - len: The length of the message from the transport.
+///   - payload: The payload provided by the caller.
+/// - Returns: `0` on success, or an error code.
+///
+/// ## C Equivalent
+///
+/// [`git_transport_message_cb()`](https://libgit2.org/docs/reference/main/transport/git_transport_message_cb.html)
+public typealias GitTransportMessageCB = @convention(c)
+(
+    UnsafePointer<CChar>?,
+    Int32,
+    UnsafeMutableRawPointer?
+) -> Int32
+
+
+
+/// The callback to create a transport.
+/// - Parameters:
+///   - out: The pointer in which to store the resulting transport.
+///   - owner: The owner of the transport. The underlying type should be `git_remote`.
+///   - payload: The payload provided by the caller.
+/// - Returns: `0` on success, or an error code.
+///
+/// ## C Equivalent
+///
+/// [`git_transport_cb()`](https://libgit2.org/docs/reference/main/transport/git_transport_cb.html)
+public typealias GitTransportCB = @convention(c)
+(
+    UnsafeMutablePointer<UnsafeMutablePointer<git_transport>?>?,
+    OpaquePointer?,
+    UnsafeMutableRawPointer?
+) -> Int32
