cf2b36565e4fee97d14147ea18f4695562129ed0
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-19 16:46:54 -0700

    Update GitApplyOptions.payload documentation

--- Sources/swift-libgit2/Apply/Apply-Structs.swift
+++ Sources/swift-libgit2/Apply/Apply-Structs.swift
@@ -31,7 +31,7 @@ public struct GitApplyOptions
     /// The callback that will be made per hunk when applying a patch.
     public var hunkCB  : GitApplyHunkCB?
     
-    /// The payload passed to both ``GitApplyOptions/deltaCB`` and
+    /// The caller-specified payload passed to both ``GitApplyOptions/deltaCB`` and
     /// ``GitApplyOptions/hunkCB``.
     public var payload : UnsafeMutableRawPointer?
     
