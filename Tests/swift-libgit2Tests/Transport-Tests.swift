3c64b3a0b6f2ef99958873f41701c7722162ba3c
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-19 13:38:17 -0700

    Add Transport tests

---
+++ Tests/swift-libgit2Tests/Transport-Tests.swift
@@ -0,0 +1,142 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import Clibgit2
+import XCTest
+@testable import SwiftLibgit2
+
+
+
+final class TransportTests: XCTestCaseStopOnFail
+{
+    // MARK: - testGitTransportCB()
+    
+    func testGitTransportCB() throws
+    {
+        let successCallback: GitTransportCB =
+        {
+            out, owner, payload in
+            
+            XCTAssertNotNil(out)
+            XCTAssertNil(owner)
+            XCTAssertNil(payload)
+            
+            return GIT_OK.rawValue
+        }
+        
+        let errorCallback: GitTransportCB =
+        {
+            out, owner, payload in
+            
+            XCTAssertNotNil(out)
+            XCTAssertNil(owner)
+            XCTAssertNil(payload)
+            
+            if let out: UnsafeMutablePointer<UnsafeMutablePointer<git_transport>?> = out
+            {
+                out.pointee = nil
+            }
+            
+            return -1
+        }
+        
+        
+        
+        var transportPointer: UnsafeMutablePointer<git_transport>? = nil
+        
+        let successCallbackResult: Int32 = successCallback(
+            &transportPointer,
+            nil,
+            nil
+        )
+        
+        XCTAssertEqual(successCallbackResult, 0)
+        
+        let errorCallbackResult: Int32 = errorCallback(
+            &transportPointer,
+            nil,
+            nil
+        )
+        
+        XCTAssertEqual(errorCallbackResult, -1)
+        
+        XCTAssertNil(transportPointer)
+    }
+    
+    
+    
+    // MARK: - testGitTransportMessageCB()
+    
+    func testGitTransportMessageCB() throws
+    {
+        let message         : String    = "123"
+        let messageCount    : Int32     = Int32(message.count)
+        
+        
+        
+        let successCallback: GitTransportMessageCB =
+        {
+            strPointer, len, payload in
+            
+            guard let str = String(optionalCString: strPointer)
+            else
+            {
+                XCTFail("The string parameter was nil.")
+                return 0
+            }
+            
+            XCTAssertEqual(str, "123")
+            XCTAssertEqual(len, 3)
+            XCTAssertNil(payload)
+            
+            return 0
+        }
+        
+        let errorCallback: GitTransportMessageCB =
+        {
+            strPointer, len, payload in
+            
+            guard let str = String(optionalCString: strPointer)
+            else
+            {
+                XCTFail("The string parameter was nil.")
+                return -1
+            }
+            
+            XCTAssertEqual(str, "123")
+            XCTAssertEqual(len, 3)
+            XCTAssertNil(payload)
+            
+            return -1
+        }
+        
+        
+        
+        message.withCString
+        {
+            cMessage in
+            
+            let successCallbackResult: Int32 = successCallback(
+                cMessage,
+                messageCount,
+                nil
+            )
+            
+            XCTAssertEqual(successCallbackResult, 0)
+            
+            let errorCallbackResult: Int32 = errorCallback(
+                cMessage,
+                messageCount,
+                nil
+            )
+            
+            XCTAssertEqual(errorCallbackResult, -1)
+        }
+    }
+}
