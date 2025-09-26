0b8ea343fa020fe27a7cb49c008758fe6a4b8e2a
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-26 11:28:57 -0700

    Update GitEnum to adopt NonOptionalCConvertible

--- Sources/SwiftLibgit2/Utilities/GitEnum.swift
+++ Sources/SwiftLibgit2/Utilities/GitEnum.swift
@@ -8,25 +8,16 @@
 //===----------------------------------------------------------------------===//
 
 /// A sendable type that can be converted to and from an associated fixed-size integer raw value.
-internal protocol GitEnum: RawRepresentable, Sendable where RawValue: FixedWidthInteger
+internal protocol GitEnum: NonOptionalCConvertible, RawRepresentable, Sendable
+    where RawValue: FixedWidthInteger
 {
-    /// The equivalent C value.
-    associatedtype T
-    
     /// Creates an instance from the equivalent C value.
     /// - Parameter cValue: The C value to use.
     ///
     /// ## Discussion
     ///
-    /// This initializer should have an `internal` access level.
+    /// This should have an `internal` access level.
     init?(
-        cValue: T
+        cValue: C
     )
-    
-    /// The equivalent C value.
-    ///
-    /// ## Discussion
-    ///
-    /// This computed property should have an `internal` access level.
-    var cValue: T { get }
 }
