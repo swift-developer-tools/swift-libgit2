18bd8e5b35ff2ae4426263f650252d2600cbcd3d
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-26 11:29:34 -0700

    Update GitOptionSet to adopt NonOptionalCConvertible

--- Sources/SwiftLibgit2/Utilities/GitOptionSet.swift
+++ Sources/SwiftLibgit2/Utilities/GitOptionSet.swift
@@ -8,25 +8,16 @@
 //===----------------------------------------------------------------------===//
 
 /// A sendable type that presents a mathematical set interface to a bit set.
-internal protocol GitOptionSet: OptionSet, Sendable where RawValue == UInt32
+internal protocol GitOptionSet: NonOptionalCConvertible, OptionSet, Sendable
+    where RawValue == UInt32
 {
-    /// The equivalent C value.
-    associatedtype T
-    
     /// Creates an instance from a raw value.
     /// - Parameter rawValue: The raw value to use.
     ///
     /// ## Discussion
     ///
-    /// This initializer should have a `public` access level.
+    /// This should have a `public` access level.
     init(
         rawValue: UInt32
     )
-    
-    /// The equivalent C value.
-    ///
-    /// ## Discussion
-    ///
-    /// This computed property should have an `internal` access level.
-    var cValue: T { get }
 }
