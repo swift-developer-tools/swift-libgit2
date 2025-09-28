cfb22ece7bb2183a4f13fef5783f3a02825378da
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-09-28 14:28:53 -0700

    Add Credential Helpers bindings

---
+++ Sources/SwiftLibgit2/Credential-Helpers/Credential-Helpers-Functions.swift
@@ -0,0 +1,47 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import Clibgit2
+
+
+
+/// Creates a new plaintext username and password credential.
+/// - Parameters:
+///   - out: The pointer in which to store the resulting credential.
+///   - url: The resource for which a credential is being demanded.
+///   - userFromURL: The username that is embedded in a `user@host` remote URL.
+///   - allowedTypes: The allowed credential types.
+///   - payload: The payload provided by the caller.
+/// - Returns: `0` on success, or an error code.
+///
+/// ## Discussion
+///
+/// This is a stock implementation of the ``GitCredentialAcquireCB`` callback, and will call
+/// ``gitCredentialUserPassPlaintextNew(out:username:password:)``, unless
+/// ``GitCredentialT/gitCredentialUserPassPlaintext`` is not an allowed type.
+///
+/// ## C Equivalent
+///
+/// [`git_credential_userpass()`](https://libgit2.org/docs/reference/main/credential_helpers/git_credential_userpass.html)
+public func gitCredentialUserPass(
+    out             : UnsafeMutablePointer<UnsafeMutablePointer<git_credential>?>,
+    url             : String,
+    userFromURL     : String?,
+    allowedTypes    : GitCredentialT,
+    payload         : UnsafeMutableRawPointer?
+) -> Int32
+{
+    return git_credential_userpass(
+        out,
+        url,
+        userFromURL,
+        allowedTypes.rawValue,
+        payload
+    )
+}
---
+++ Sources/SwiftLibgit2/Credential-Helpers/Credential-Helpers-Structs.swift
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import Clibgit2
+
+
+
+/// The payload for plaintext username/password authentication.
+///
+/// ## Discussion
+///
+/// This struct is provided for documentation purposes, but is not used by other bindings.
+///
+/// All bindings use
+/// [`UnsafeMutableRawPointer`](https://developer.apple.com/documentation/swift/unsafemutablerawpointer)
+/// payloads.
+///
+/// ## C Equivalent
+///
+/// [`git_credential_userpass_payload`](https://libgit2.org/docs/reference/main/credential_helpers/git_credential_userpass_payload.html)
+public struct GitCredentialUserPassPayload: GitStruct
+{
+    /// The username of the credential.
+    public let username : String
+    
+    /// The password of the credential.
+    public let password : String
+
+    
+    
+    /// Creates a ``GitCredentialUserPassPayload`` instance from a
+    /// `git_credential_userpass_payload` instance.
+    /// - Parameter payload: The `git_credential_userpass_payload` instance to use.
+    ///
+    /// ## Discussion
+    ///
+    /// ``username`` and ``password`` default to empty strings if unexpected values are
+    /// encountered, although this should never occur.
+    internal init(
+        cValue payload: git_credential_userpass_payload
+    )
+    {
+        self.username   = String(optionalCString: payload.username) ?? ""
+        self.password   = String(optionalCString: payload.password) ?? ""
+    }
+}
