cd8d04522622e26bdf4dc440eef95a6a56dc4ea4
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-01 11:10:31 -0700

    Update `GitRemoteCallbacks.updateTips` documentation

--- Sources/SwiftLibgit2/Remote/Remote-Structs.swift
+++ Sources/SwiftLibgit2/Remote/Remote-Structs.swift
@@ -72,7 +72,7 @@ public struct GitRemoteCallbacks: GitStructMutable, ThrowingCConvertible
     ///
     /// The default value is `nil`.
     ///
-    /// This is deprecated in libgit2 and will be removed in the next major release.
+    /// - Warning: This is deprecated in libgit2 and will be removed in the next major release.
     /// Use ``updateRefs`` instead.
     public var updateTips           : GitRemoteUpdateTipsCB?            = nil
     
