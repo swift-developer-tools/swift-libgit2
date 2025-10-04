5f1c00ae01ffc02b2bc4b9f244e3135a3389a4b6
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-03 19:11:16 -0700

    Add Email bindings

---
+++ Sources/SwiftLibgit2/Email/Email-Functions.swift
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import CLibgit2
+
+
+
+/// Creates a diff from the given commit in `mbox` format to send via email.
+/// - Parameters:
+///   - out: The buffer into which the email patch should be written
+///   - commit: The commit for which to create a patch. The underlying type must be `git_commit`. 
+///   - opts: The options for formatting generated emails.
+/// - Returns: `0` on success, or an error code.
+///
+/// ## Discussion
+///
+/// - Important: The given commit must not be a merge commit.
+///
+/// ## C Equivalent
+///
+/// [`git_email_create_from_commit()`](https://libgit2.org/docs/reference/main/email/git_email_create_from_commit.html)
+public func gitEmailCreateFromCommit(
+    out     : inout GitBuf,
+    commit  : OpaquePointer,
+    opts    : GitEmailCreateOptions?
+) -> Int32
+{
+    return withCConversion
+    {
+        return try out.withMutatingCValue
+        {
+            cOut in
+            
+            return try opts.withOptionalCValue
+            {
+                cOpts in
+                
+                return git_email_create_from_commit(
+                    cOut,
+                    commit,
+                    cOpts
+                )
+            }
+        }
+    }
+}
