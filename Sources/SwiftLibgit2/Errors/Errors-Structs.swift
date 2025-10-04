e40c68e78a427c449bd1e900645962398cba6fca
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-04 10:39:23 -0700

    Add Errors bindings

---
+++ Sources/SwiftLibgit2/Errors/Errors-Structs.swift
@@ -0,0 +1,62 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import CLibgit2
+
+
+
+/// The details of an error.
+///
+/// ## C Equivalent
+///
+/// [`git_error`](https://libgit2.org/docs/reference/main/errors/git_error.html)
+public struct GitError: GitStructReadable, WithCConvertible
+{
+    /// The error message.
+    public let message  : String?
+    
+    /// The category of the error.
+    public let klass    : GitErrorT
+    
+    
+    
+    /// Creates a ``GitError`` instance from a `git_error` instance.
+    /// - Parameter error: The `git_error` instance to use.
+    internal init(
+        cValue error: git_error
+    )
+    {
+        self.message    = String(optionalCString: error.message)
+        self.klass      = GitErrorT(rawValue: error.klass)
+    }
+    
+    
+    
+    /// Calls the given closure with a pointer to a `git_error` instance.
+    /// - Parameter body: The closure to call.
+    /// - Returns: The return value of the given closure.
+    /// - Throws: An `NSError` if the conversion failed.
+    internal func withCValue<T>(
+        _ body: (UnsafeMutablePointer<git_error>) throws -> T
+    ) throws -> T
+    {
+        var error = git_error()
+        
+        error.klass = klass.rawValue
+        
+        return try message.withOptionalMutableCString
+        {
+            cMessage in
+            
+            error.message = cMessage
+            
+            return try body(&error)
+        }
+    }
+}
---
+++ Sources/SwiftLibgit2/SwiftLibgit2.docc/Errors.md
@@ -0,0 +1,19 @@
+# Errors
+
+Error information and management.
+
+## Topics
+
+### Structs
+
+- ``GitError``
+
+### Enums
+
+- ``GitErrorCode``
+- ``GitErrorT``
+
+### Functions
+
+- ``gitErrorLast()``
+
