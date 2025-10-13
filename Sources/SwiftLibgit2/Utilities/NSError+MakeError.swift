f7d42f782513da657e014edd4d7abbc1c8fdeb5b
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-13 10:26:08 -0700

    Update `NSError.makeError(code:message:)` documentation

--- Sources/SwiftLibgit2/Utilities/NSError+MakeError.swift
+++ Sources/SwiftLibgit2/Utilities/NSError+MakeError.swift
@@ -22,9 +22,11 @@ internal extension NSError
     ///
     /// ## Discussion
     ///
-    /// libgit2 result codes use a type of `Int32`, but `NSError` expects
-    /// `Int`. ``code`` can be safely cast from `Int32` to `Int`, since this
-    /// is a widening conversion.
+    /// libgit2 result codes use a type of `Int32`, but `NSError` uses `Int`.
+    /// ``code`` can be safely cast from `Int32` to `Int`, since this is a
+    /// widening conversion. `Int` is guaranteed to be at least as large as
+    /// `Int32` on all platforms (`Int` will be the same size as `Int32` on
+    /// 32-bit platforms and the same size as `Int64` on 64-bit platforms).
     static func makeError(
         code    : Int32,
         message : String
