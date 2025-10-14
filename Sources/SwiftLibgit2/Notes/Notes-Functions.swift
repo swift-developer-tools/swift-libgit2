91ff33077f6b65ad1bcaf14cbc9a00133286bb40
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-14 08:51:28 -0700

    Update `gitNoteForEach()` `payload` parameter documentation

--- Sources/SwiftLibgit2/Notes/Notes-Functions.swift
+++ Sources/SwiftLibgit2/Notes/Notes-Functions.swift
@@ -594,7 +594,7 @@ public func gitNoteDefaultRef(
 ///   - notesRef: The canonical name of the reference to use. Pass `nil` to
 ///   use `refs/notes/commits`.
 ///   - noteCB: The callback to invoke for each note.
-///   - payload: The payload provided by the caller.
+///   - payload: The payload to pass to `noteCB`.
 /// - Returns: A ``GitErrorCode`` instance.
 ///
 /// ## C Equivalent
