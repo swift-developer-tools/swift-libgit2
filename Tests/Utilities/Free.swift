b6bac5f991b9ee1c5f53958193d0efe877a3b8d9
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-22 15:23:10 -0700

    Add Repository tests

--- Tests/Utilities/Free.swift
+++ Tests/Utilities/Free.swift
@@ -84,4 +84,22 @@ enum Free
         
         git_tree_free(tree)
     }
+    
+    
+    
+    /// Frees the memory allocated for the given `git_worktree` instance.
+    /// - Parameter worktree: The worktree to free. The underlying type must
+    /// be `git_worktree`.
+    static func freeWorktree(
+        _ worktree: OpaquePointer?
+    )
+    {
+        guard let worktree: OpaquePointer = worktree
+        else
+        {
+            return
+        }
+        
+        git_worktree_free(worktree)
+    }
 }
--- Tests/Utilities/Repository.swift
+++ Tests/Utilities/Repository.swift
@@ -606,43 +606,68 @@ internal extension Repository
     
     
     /// Calls the given closure with a ``Repository`` instance.
-    /// - Parameter body: The closure to call.
+    /// - Parameters:
+    ///   - options: The repository initialization options to use.
+    ///   - isBare: Whether to initialize a bare repository.
+    ///   - body: The closure to call.
     /// - Returns: The return value of the given closure.
     /// - Throws: An error if an operation fails.
     static func withRepository<T>(
-        _ body: (Repository) throws -> T
+        options : GitRepositoryInitOptions?     = nil,
+        isBare  : Bool                          = false,
+        _ body  : (Repository) throws -> T
     ) throws -> T
     {
-        var repositoryPointer   : OpaquePointer?    = nil
-        let url                 : URL               = try createTemporaryDirectory(named: "SwiftLibgit2Tests")
+        var repoPointer : OpaquePointer?    = nil
+        let url         : URL               = try createTemporaryDirectory(named: "SwiftLibgit2Tests")
         
         defer
         {
-            Free.freeRepository(repositoryPointer)
+            Free.freeRepository(repoPointer)
             
             try? FileManager.default.removeItem(at: url)
         }
         
         
         
-        let repositoryInitResult: Int32 = git_repository_init(
-            &repositoryPointer,
-            url.path,
-            0
-        )
-        
-        XCTAssertOK(GitErrorCode(rawValue: repositoryInitResult))
-        
-        guard let repositoryPointer: OpaquePointer = repositoryPointer
+        if let options: GitRepositoryInitOptions = options
+        {
+            let repoInitExtResult: GitErrorCode = gitRepositoryInitExt(
+                out:        &repoPointer,
+                repoPath:   url.path(),
+                opts:       options
+            )
+            
+            XCTAssertOK(repoInitExtResult)
+        }
         else
         {
-            throw NSError.makeError("Failed to initialize repository.")
+            let repoInitResult: Int32 = git_repository_init(
+                &repoPointer,
+                url.path(),
+                isBare ? 1 : 0
+            )
+            
+            XCTAssertOK(GitErrorCode(rawValue: repoInitResult))
+        }
+        
+        guard let repoPointer: OpaquePointer = repoPointer
+        else
+        {
+            throw NSError.makeError("The repository pointer was nil.")
         }
         
         let repository = Repository(
             url:        url,
-            pointer:    repositoryPointer
+            pointer:    repoPointer
         )
+        
+        
+        
+        if isBare
+        {
+            return try body(repository)
+        }
         
         
         
@@ -652,6 +677,7 @@ internal extension Repository
             message:    "Initial commit"
         )
         
+        try createBlameData(in: repository)
         
         
         
@@ -682,8 +708,6 @@ internal extension Repository
         }
         
         
-        
-        try createBlameData(in: repository)
         
         return try body(repository)
     }
