d4a01e25f4a331241839f1a37acb8647dd77d35a
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-22 19:33:21 -0700

    Add Revert bindings

---
+++ Sources/SwiftLibgit2/Revert/Revert-Macros.swift
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+/// The current version for ``GitRevertOptions``.
+///
+/// ## C Equivalent
+///
+/// [`GIT_REVERT_OPTIONS_VERSION`](https://libgit2.org/docs/reference/main/revert/GIT_REVERT_OPTIONS_VERSION.html)
+public let gitRevertOptionsVersion: UInt32 = 1
