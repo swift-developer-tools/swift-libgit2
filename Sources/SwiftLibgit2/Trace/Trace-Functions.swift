a4ec45618feffa5fb1c643c1db6cb527337bbd41
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-25 11:55:20 -0700

    Add Trace bindings

---
+++ Sources/SwiftLibgit2/Trace/Trace-Functions.swift
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import CLibgit2
+
+
+
+/// Sets the system tracing configuration to the given level with the given
+/// callback.
+/// - Parameters:
+///   - level: The trace level to set.
+///   - cb: The callback to invoke with trace data.
+/// - Returns: A ``GitErrorCode`` instance.
+///
+/// ## C Equivalent
+///
+/// [`git_trace_set()`](https://libgit2.org/docs/reference/main/trace/git_trace_set.html)
+public func gitTraceSet(
+    level   : GitTraceLevelT,
+    cb      : GitTraceCB
+) -> GitErrorCode
+{
+    return withCConversion
+    {
+        return git_trace_set(
+            level.cValue(),
+            cb
+        )
+    }
+}
