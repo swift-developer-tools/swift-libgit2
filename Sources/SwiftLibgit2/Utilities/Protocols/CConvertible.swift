78670def95dba8c3e0bd3b15932353d05daf4a47
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-26 07:31:20 -0700

    Move `withCConversion(_:)` to separate file

--- Sources/SwiftLibgit2/Utilities/Protocols/CConvertible.swift
+++ Sources/SwiftLibgit2/Utilities/Protocols/CConvertible.swift
@@ -7,11 +7,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-import CLibgit2
-import Foundation
-
-
-
 /// A type that can be converted to the equivalent C value.
 ///
 /// ## Discussion
@@ -236,41 +231,4 @@ internal extension Optional where Wrapped: WithCConvertible
                 return try wrapped.withCValue(body)
         }
     }
-}
-
-
-
-// MARK: - Utilities
-
-/// Calls the given closure within a `do`/`catch` block and returns
-/// ``GitErrorCode/gitEUser`` if an error is thrown.
-/// - Parameter body: The closure to call.
-/// - Returns: The return value of the given closure or the `code` property
-/// of a thrown `NSError`, converted to a ``GitErrorCode`` instance, or
-/// ``GitErrorCode/gitEUser`` for any other thrown error.
-internal func withCConversion(
-    _ body: () throws -> Int32
-) -> GitErrorCode
-{
-    var result: Int32 = GitErrorCode.gitEUser.rawValue
-    
-    do
-    {
-        result = try body()
-    }
-    catch let error as NSError
-    {
-        if
-            error.code >= Int32.min,
-            error.code <= Int32.max
-        {
-            result = Int32(error.code)
-        }
-    }
-    catch
-    {
-        /// Non-`NSError` cases return the default value.
-    }
-    
-    return GitErrorCode(rawValue: result)
 }
