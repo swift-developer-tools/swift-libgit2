47796ae80adba6911e14c524c8c5ac5534e7e0cb
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-29 14:23:53 -0700

    Move `isOK(_:)` to XCTestAssertions file

--- Tests/Utilities/XCTestAssertions.swift
+++ Tests/Utilities/XCTestAssertions.swift
@@ -13,6 +13,30 @@ import XCTest
 
 
 
+// MARK: - Utilities
+
+/// Checks whether the given libgit2 operation result is ``GitErrorCode/gitOK``,
+/// and clears the last libgit2 error otherwise.
+/// - Parameter errorCode: The libgit2 operation result to check.
+/// - Returns: Whether the given libgit2 operation result is
+/// ``GitErrorCode/gitOK``.
+func isOK(
+    _ errorCode: GitErrorCode
+) -> Bool
+{
+    let isOK: Bool = errorCode == .gitOK
+    
+    if !isOK
+    {
+        // TODO: Replace when `git_error_clear()` has a binding, and remove CLibgit2 import.
+        git_error_clear()
+    }
+    
+    return isOK
+}
+
+
+
 // MARK: - Result Codes
 
 /// Asserts that the given libgit2 operation result code is
