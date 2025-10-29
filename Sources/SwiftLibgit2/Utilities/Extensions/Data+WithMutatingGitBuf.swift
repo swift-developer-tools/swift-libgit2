c2cf40401a73cb24fb6973f46f5198f7d79763db
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-28 17:29:32 -0700

    Remove unnecessary `Data.withMutatingGitBuf(_:)` documentation
    All mutating API methods mutate the receiver only if the operation
    was successful, so it is not necessary to specifically mention it.

--- Sources/SwiftLibgit2/Utilities/Extensions/Data+WithMutatingGitBuf.swift
+++ Sources/SwiftLibgit2/Utilities/Extensions/Data+WithMutatingGitBuf.swift
@@ -18,12 +18,6 @@ internal extension Data
     /// and updates the receiver with any changes made by the closure.
     /// - Parameter body: The closure to call.
     /// - Returns: The return value of the given closure.
-    ///
-    /// ## Discussion
-    ///
-    /// This method will only mutate the receiver if the operation was
-    /// successful and `git_buf->ptr` is not `nil`. The pointer should
-    /// generally not be `nil` after a successful operation.
     mutating func withMutatingGitBuf<T>(
         _ body: (UnsafeMutablePointer<git_buf>) throws -> T
     ) throws -> T
