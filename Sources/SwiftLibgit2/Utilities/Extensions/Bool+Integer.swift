55ff3cc586a3c5accc562093a47d9bdeb1a47772
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-10-29 08:23:23 -0700

    Move Bool extensions to same file

--- Sources/SwiftLibgit2/Utilities/Extensions/Bool+Integer.swift
+++ Sources/SwiftLibgit2/Utilities/Extensions/Bool+Integer.swift
@@ -78,4 +78,26 @@ internal extension Bool
     {
         self = cValue != 0
     }
+    
+    
+    
+    /// Calls the given closure with a mutable pointer to a signed 32-bit
+    /// integer, and updates the receiver with any changes made by the closure.
+    /// - Parameter body: The closure to call.
+    /// - Returns: The return value of the given closure.
+    mutating func withMutatingBool<T>(
+        _ body: (UnsafeMutablePointer<Int32>) throws -> T
+    ) rethrows -> T
+    {
+        var intValue: Int32 = self.int32Value
+        
+        let result: T = try body(&intValue)
+        
+        if isSuccess(result)
+        {
+            self = Bool(intValue)
+        }
+        
+        return result
+    }
 }
