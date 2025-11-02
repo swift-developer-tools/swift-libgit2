e87346d2d1563da90af69ae95b3fcfa1d286c175
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-11-02 13:57:30 -0800

    Standardize Transport callback tests

--- Tests/swift-libgit2Tests/Transport-Tests.swift
+++ Tests/swift-libgit2Tests/Transport-Tests.swift
@@ -17,127 +17,40 @@ final class TransportTests: XCTestCaseStopOnFail
 {
     func testGitTransportCB() throws
     {
-        let successCallback: GitTransportCB =
+        let transportCB: GitTransportCB =
         {
-            out, owner, payload in
-            
-            XCTAssertNotNil(out)
-            XCTAssertNil(owner)
-            XCTAssertNil(payload)
+            _, _, _ in
             
             return GitErrorCode.gitOK.rawValue
         }
         
-        
-        
-        let errorCallback: GitTransportCB =
-        {
-            out, owner, payload in
-            
-            XCTAssertNotNil(out)
-            XCTAssertNil(owner)
-            XCTAssertNil(payload)
-            
-            if let out: UnsafeMutablePointer<UnsafeMutablePointer<git_transport>?>
-                = out
-            {
-                out.pointee = nil
-            }
-            
-            return GitErrorCode.gitUnknown(-123).rawValue
-        }
-        
-        
-        
-        var transportPointer: UnsafeMutablePointer<git_transport>? = nil
-        
-        let successCallbackResult: Int32 = successCallback(
-            &transportPointer,
+        let transportCBResult: Int32 = transportCB(
+            nil,
             nil,
             nil
         )
         
-        XCTAssertEqual(successCallbackResult, GitErrorCode.gitOK.rawValue)
-        
-        let errorCallbackResult: Int32 = errorCallback(
-            &transportPointer,
-            nil,
-            nil
-        )
-        
-        XCTAssertEqual(errorCallbackResult, -123)
-        
-        XCTAssertNil(transportPointer)
+        XCTAssertOK(GitErrorCode(rawValue: transportCBResult))
     }
     
     
     
     func testGitTransportMessageCB() throws
     {
-        let message         : String    = "abc"
-        let messageCount    : Int32     = Int32(message.count)
-        
-        
-        
-        let successCallback: GitTransportMessageCB =
+        let transportMessageCB: GitTransportMessageCB =
         {
-            strPointer, len, payload in
-            
-            guard let str = String(optionalCString: strPointer)
-            else
-            {
-                XCTFail("The string pointer was nil.")
-                return GitErrorCode.gitUnknown(-123).rawValue
-            }
-            
-            XCTAssertEqual(str, "abc")
-            XCTAssertEqual(len, 3)
-            XCTAssertNil(payload)
+            _, _, _ in
             
             return GitErrorCode.gitOK.rawValue
         }
         
         
+        let transportMessageCBResult: Int32 = transportMessageCB(
+            nil,
+            0,
+            nil
+        )
         
-        let errorCallback: GitTransportMessageCB =
-        {
-            strPointer, len, payload in
-            
-            guard let str = String(optionalCString: strPointer)
-            else
-            {
-                XCTFail("The string pointer was nil.")
-                return GitErrorCode.gitUnknown(-123).rawValue
-            }
-            
-            XCTAssertEqual(str, "abc")
-            XCTAssertEqual(len, 3)
-            XCTAssertNil(payload)
-            
-            return GitErrorCode.gitUnknown(-123).rawValue
-        }
-        
-        
-        
-        message.withCString
-        {
-            cMessage in
-            
-            let successCallbackResult: Int32 = successCallback(
-                cMessage,
-                messageCount,
-                nil
-            )
-            
-            XCTAssertEqual(successCallbackResult, GitErrorCode.gitOK.rawValue)
-            
-            let errorCallbackResult: Int32 = errorCallback(
-                cMessage,
-                messageCount,
-                nil
-            )
-            
-            XCTAssertEqual(errorCallbackResult, -123)
-        }
+        XCTAssertOK(GitErrorCode(rawValue: transportMessageCBResult))
     }
 }
