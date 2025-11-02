3c4c96e0b70ed9265b036312190b011061a9ad0a
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-11-02 09:16:22 -0800

    Add Stream (Advanced) bindings

---
+++ Sources/SwiftLibgit2/Stream-Advanced/Stream-Advanced-Callbacks.swift
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libgit2 open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import CLibgit2
+
+
+
+/// The callback invoked to create a new connection to the given host and port.
+/// - Parameters:
+///   - out: The pointer in which to store the stream.
+///   - host: The name of the host to which to connect the stream.
+///   - port: The port to which to connect the stream.
+/// - Returns: `0` on success, or an error code.
+///
+/// ## Discussion
+///
+/// - Warning: This is deprecated in libgit2 and will be removed in the next
+/// major release. Use ``GitStreamRegistration/Initialize`` instead.
+///
+/// ## C Equivalent
+///
+/// [`git_stream_cb()`](https://libgit2.org/docs/reference/main/sys/stream/git_stream_cb.html)
+public typealias GitStreamCB = @convention(c)
+(
+    UnsafeMutablePointer<UnsafeMutablePointer<git_stream>?>?,
+    UnsafePointer<CChar>?,
+    UnsafePointer<CChar>?
+) -> Int32
