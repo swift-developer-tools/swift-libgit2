d7a3f5ed6b291b427eb29aaec931e09362371c05
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2025-11-02 15:23:15 -0800

    Fix `GitODBForEachCB` C equivalent documentation

--- Sources/SwiftLibgit2/ODB/ODB-Callbacks.swift
+++ Sources/SwiftLibgit2/ODB/ODB-Callbacks.swift
@@ -13,13 +13,13 @@ import CLibgit2
 
 /// The callback invoked for each object.
 /// - Parameters:
-///   - blobID: The ID of an object in the object database.
+///   - id: The ID of the object in the object database.
 ///   - payload: The payload provided by the caller.
 /// - Returns: `0` on success, or an error code.
 ///
 /// ## C Equivalent
 ///
-/// [`git_note_foreach_cb()`](https://libgit2.org/docs/reference/main/notes/git_note_foreach_cb.html)
+/// [`git_odb_foreach_cb()`](https://libgit2.org/docs/reference/main/odb/git_odb_foreach_cb.html)
 public typealias GitODBForEachCB = @convention(c)
 (
     UnsafePointer<git_oid>?,
